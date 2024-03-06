e7fb823caaa0aaaac140d6c8babb98a25c465a61
Ashu525 <ashutoshshrivastava692@gmail.com>
2024-03-07 00:39:23 +0530

    mongoose and mongodb setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const { createHandler } = require('graphql-http/lib/use/express');
-const schema = require('./schema/schema')
+const express = require("express");
+const { createHandler } = require("graphql-http/lib/use/express");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use('/graphql', createHandler({schema}))
+mongoose.connect(
+  "mongodb+srv://ashu525:ashu525@cluster0.cqxoht4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use("/graphql", createHandler({ schema }));
 
 app.listen(4000, () => {
-    console.log('Server is running on port 4000')
-})
+  console.log("Server is running on port 4000");
+});
